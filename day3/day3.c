353dfd3d9cb630984e29f52fcd1b551f99d5130c
Yorick Rommers <yorick-rommers@hotmail.com>
2017-12-04 02:51:44 +0100

    Day 3 part 2

--- day3/day3.c
+++ day3/day3.c
@@ -7,7 +7,7 @@
 void print_grid(int size, int grid[size][size]) {
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-            printf("%d ", grid[i][j]);
+            printf("%d\t", grid[i][j]);
         }
         printf("\n");
     }
@@ -49,21 +49,7 @@ int next(int dir) {
     return -1;
 }
 
-int day3(int argc, char** argv) {
-    (void) argc;
-    (void) argv;
-
-    int to_find = 347991;
-
-    size_t MAX = to_find;
-
-    int grid_size = 1;
-    while ((grid_size * grid_size) < MAX) {
-        grid_size+=2; // so that we end up with an uneven grid.
-    }
-
-    // printf("Grid size is calculated to be %d\n", grid_size);
-
+void part1_d3(int grid_size, int to_find) {
     int grid[grid_size][grid_size];
 
     // fill grid with zeros.
@@ -124,11 +110,7 @@ int day3(int argc, char** argv) {
                 times_ran = 0;
             }
         }
-
-
     }
-    // printf("\n");
-    // print_grid(grid_size, grid);
 
     // find value in grid.
     struct indexes id = find_in_grid(grid_size, grid, to_find);
@@ -136,8 +118,172 @@ int day3(int argc, char** argv) {
 
     // calc distance.
     printf("Distance to 1 is %d\n", (abs(half - id.i) + abs(half - id.j)));
+}
 
+enum {
+    R = 0,
+    TR = 1,
+    T = 2,
+    TL = 3,
+    L = 4,
+    BL = 5,
+    B = 6,
+    BR = 7,
+};
 
+void part2_d3(int grid_size, int to_find) {
+    int grid[grid_size][grid_size];
+
+    // fill grid with zeros.
+    for (int i = 0; i < grid_size; i++) {
+        for (int j = 0; j < grid_size; j++) {
+            grid[i][j] = 0;
+        }
+    }
+
+    // print_grid(grid_size, grid);
+
+    int half = grid_size / 2;
+    // printf("Half is %d\n", half);
+    grid[half][half] = 1;
+    // print_grid(grid_size, grid);
+
+    int posi = half;
+    int posj = half;
+    int dir = RIGHT;
+
+    int times_ran = 0;
+    int times_replaced = 0;
+    int count = 1;
+
+    int print_once = 1;
+
+    for (int i = 0; i < grid_size*grid_size; i++) {
+        // check adjecent tiles and summate.
+        int cur = grid[posi][posj];
+        if (cur == 0) {
+            int sum = 0;
+            for( int t = 0; t < 8; t++) {
+                int i_to_check = -1;
+                int j_to_check = -1;
+                switch (t) {
+                    case R:
+                        // check right tile.
+                        i_to_check = posi;
+                        j_to_check = posj + 1;
+                        break;
+                    case TR:
+                        i_to_check = posi - 1;
+                        j_to_check = posj + 1;
+                        break;
+                    case T:
+                        i_to_check = posi - 1;
+                        j_to_check = posj;
+                        break;
+                    case TL:
+                        i_to_check = posi - 1;
+                        j_to_check = posj - 1;
+                        break;
+                    case L:
+                        i_to_check = posi;
+                        j_to_check = posj - 1;
+                        break;
+                    case BL:
+                        i_to_check = posi + 1;
+                        j_to_check = posj - 1;
+                        break;
+                    case B:
+                        i_to_check = posi + 1;
+                        j_to_check = posj;
+                        break;
+                    case BR:
+                        i_to_check = posi + 1;
+                        j_to_check = posj + 1;
+                        break;
+                }
+                // if out of bounds, don't summate
+                if (i_to_check >= grid_size || i_to_check < 0) {
+                    continue;
+                }
+
+                if (j_to_check >= grid_size || j_to_check < 0) {
+                    continue;
+                }
+
+                int val = grid[i_to_check][j_to_check];
+                if (val != 0) {
+                    sum+= val;
+                }
+            }
+            if (sum > to_find && print_once) {
+                printf("Larger value written is %d\n", sum);
+                print_once = 0;
+            }
+            grid[posi][posj] = sum;
+        }
+
+        switch (dir) {
+            case RIGHT:
+                // check right
+                posj++;
+                break;
+            case UP:
+                posi--;
+                break;
+            case LEFT:
+                posj--;
+                break;
+            case DOWN:
+                posi++;
+                break;
+        }
+        /*
+        run twice
+            run replace once, then switchdir
+        run twice
+            run replace twice, then switchdir
+        run twice
+            run replace thrice, then switchdir
+        etc.
+        */
+        times_replaced++; // we've replaced one
+        if (times_replaced == count) { // if we've replaced count times
+            times_ran++; // we've ran once more
+            times_replaced = 0; // reset replaced
+            dir = next(dir); // switch dir
+            if (times_ran == 2) {
+                count++;
+                times_ran = 0;
+            }
+        }
+    }
+    // print_grid(grid_size, grid);
+}
+
+int day3(int argc, char** argv) {
+    (void) argc;
+    (void) argv;
+
+    int to_find = 347991;
+
+    size_t MAX = to_find;
+
+    int grid_size = 1;
+    while ((grid_size * grid_size) < MAX) {
+        grid_size+=2; // so that we end up with an uneven grid.
+    }
+
+    // printf("Grid size is calculated to be %d\n", grid_size);
+
+    // printf("\n");
+    // print_grid(grid_size, grid);
+
+    printf("Part 1\n");
+    part1_d3(grid_size, to_find);
+    printf("----\n");
+    printf("Part 2\n");
+    part2_d3(grid_size, to_find);
+    printf("----\n");
 
     return 0;
 }
